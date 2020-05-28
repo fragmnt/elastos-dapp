95fc1bf60dea510996d617017463b4b49bb6403c
gsims <4537336-sznlabs@users.noreply.gitlab.com>
2020-05-28 02:22:26 -0400

    ...

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class IndexPg extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {}
+	}
+
+	componentDidMount = () => {
+
+	}
+
+	render() {
+		return (<>Hello!</>);
+	}
+
+}
+
+export default IndexPg;
