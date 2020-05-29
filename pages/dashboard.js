6e8a0b28cfc9a5c94a41d813697aad08b9f485a0
gsims <4537336-sznlabs@users.noreply.gitlab.com>
2020-05-29 01:40:05 -0400

    ...

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,12 +3,25 @@ import Link from 'next/link';
 
 class DashboardPg extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            walletAddress: null,
+        }
+    }
+
+    componentDidMount = async () => {
+        var accountAddress = await localStorage.getItem('account');
+        accountAddress ? this.setState({ walletAddress: accountAddress}) : null
+    };
+
     render() {
         return (
             <>
                 <Link href="/">
                     return to homepage
                 </Link>
+                {this.state.walletAddress ? this.state.walletAddress : null }
                 <h2>Dashboard</h2>
             </>
         )
